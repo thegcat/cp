055db9ccd27712e158e734bf37568142e2010423
Felix <felix.schaefer@tu-dortmund.de>
2013-11-01 23:17:27 +0100

    Allow setting the name of a project on create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ require "chili_project/project_creator"
 class ProjectsController < ApplicationController
   def create
     begin
-      project = project_creation_service.process(params[:project])
+      project = project_creation_service.process(project_params)
     rescue ChiliProject::ProjectCreator::CreationError
       head :status => :unprocessable_entity and return
     end
@@ -12,6 +12,10 @@ class ProjectsController < ApplicationController
   end
 
   private
+
+  def project_params
+    params.require(:project).permit(:name) if params[:project].present?
+  end
 
   def project_creation_service
     ChiliProject::ProjectCreator.new(Project)
