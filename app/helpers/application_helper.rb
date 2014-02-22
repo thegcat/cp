3de268277cfad437802e242377632f2cbf03be91
Felix <felix.schaefer@tu-dortmund.de>
2014-02-22 20:41:01 +0100

    Refine alert-boxes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def render_flashes(*names)
+    capture do
+      names.each {|f| concat render_flash(f)}
+    end
+  end
+
+  def render_flash(name)
+    message = flash[name]
+    return unless message.present?
+    content_tag :div, class: name, :'data-alert' => true do
+      concat message
+      concat content_tag :a, '&times;'.html_safe, href: '#', class: :close
+    end
+  end
 end
