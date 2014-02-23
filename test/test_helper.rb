c037fe3b13bafc805dccfb6e3c66500cb961151f
Felix <felix.schaefer@tu-dortmund.de>
2014-02-23 23:31:14 +0100

    Clean up test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,3 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/rails/capybara'
-
-# To add Capybara feature tests add `gem "minitest-rails-capybara"`
-# to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
