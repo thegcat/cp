de7b366af3496b39089fe655ffe34597d5602463
Felix <felix.schaefer@tu-dortmund.de>
2014-04-15 08:13:16 +0200

    Remove fixtures loading code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,3 @@ require 'minitest/rails/capybara'
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
 # require "minitest/rails/capybara"
-
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
-end
